ffd37ed68d095d8124726059ccee2bfdbe5f2d6d
Sergey <bugakov.sa.88@gmail.com>
2017-12-17 21:39:32 +0300

    sessionId cookie

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,11 +1,20 @@
 import java.nio.file.Paths
+import java.util.concurrent.ConcurrentHashMap
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.headers.HttpCookie
 import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
 import akka.http.scaladsl.server.{HttpApp, Route}
 import spray.json.DefaultJsonProtocol._
 
+import scala.util.Random
+
 object Server extends HttpApp {
+
+  final case class UserSession(id: String, userName: String)
+
+  val rnd = Random
+  val sessions = new ConcurrentHashMap[String, UserSession]()
 
   final case class StatResponse(usersCount: Int, recordsCount: Int)
 
@@ -19,7 +28,31 @@ object Server extends HttpApp {
     } ~ get {
       path("api" / "login") {
         parameters('login.as[String]) { inputLogin =>
-          redirect("/page/main.html", StatusCodes.PermanentRedirect)
+          val newSessionId = rnd.nextLong.toString
+          sessions.put(newSessionId, UserSession(newSessionId, inputLogin))
+          setCookie(HttpCookie("sessionId", newSessionId)) {
+            redirect("/page/main.html", StatusCodes.PermanentRedirect)
+          }
+        }
+      }
+    } ~ delete {
+      path("api" / "logout") {
+        cookie("sessionId") { id => {
+          sessions.remove(id.value)
+          deleteCookie("sessionId") {
+            complete("Success logout")
+          }
+        }
+        }
+      }
+    } ~ get {
+      path("api" / "login" / "current") {
+        cookie("sessionId") { id => {
+          Option(sessions.get(id.value)) match {
+            case None => complete("Unknown")
+            case Some(session) => complete(session.userName)
+          }
+        }
         }
       }
     } ~ get {
